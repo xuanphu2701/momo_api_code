e424e0ffc9d02af03c1d96c4af0ecc84435a5b53
xuanphu2701 <phuwork2701@gmai.com>
2024-02-28 11:36:46 +0700

    initial commit

---
+++ test_momo_api.js
@@ -0,0 +1,87 @@
+//change this var 
+var partnerCode = 'MOMOYQBV20240223_TEST';
+var accessKey = 'rLngVVmkvIs0DbBt';
+var secretKey = 'hbUnrAnMUaAkQSlSHKB7iPSsYHD8fGi6';
+
+var orderInfo = 'pay with MoMo';
+var redirectUrl = 'https://momo.vn';
+var ipnUrl = 'https://momo.vn';
+var requestType = "captureWallet";
+var amount = '50000';
+var orderId = partnerCode + new Date().getTime();
+var requestId = orderId;
+var extraData ='';
+var orderGroupId ='';
+var autoCapture =true;
+var lang = 'vi';
+
+
+//before sign HMAC SHA256 with format
+//accessKey=$accessKey&amount=$amount&extraData=$extraData&ipnUrl=$ipnUrl&orderId=$orderId&orderInfo=$orderInfo&partnerCode=$partnerCode&redirectUrl=$redirectUrl&requestId=$requestId&requestType=$requestType
+var rawSignature = "accessKey=" + accessKey + "&amount=" + amount + "&extraData=" + extraData + "&ipnUrl=" + ipnUrl + "&orderId=" + orderId + "&orderInfo=" + orderInfo + "&partnerCode=" + partnerCode + "&redirectUrl=" + redirectUrl + "&requestId=" + requestId + "&requestType=" + requestType;
+//puts raw signature
+console.log("--------------------ORDER ID----------------")
+console.log(orderId)
+console.log("--------------------RAW SIGNATURE----------------")
+console.log(rawSignature)
+//signature
+const crypto = require('crypto');
+var signature = crypto.createHmac('sha256', secretKey)
+    .update(rawSignature)
+    .digest('hex');
+console.log("--------------------SIGNATURE----------------")
+console.log(signature)
+
+//json object send to MoMo endpoint
+const requestBody = JSON.stringify({
+    partnerCode : partnerCode,
+    partnerName : "Test",
+    storeId : "MomoTestStore",
+    requestId : requestId,
+    amount : amount,
+    orderId : orderId,
+    orderInfo : orderInfo,
+    redirectUrl : redirectUrl,
+    ipnUrl : ipnUrl,
+    lang : lang,
+    requestType: requestType,
+    autoCapture: autoCapture,
+    extraData : extraData,
+    orderGroupId: orderGroupId,
+    signature : signature
+});
+//Create the HTTPS objects
+const https = require('https');
+const options = {
+    hostname: 'test-payment.momo.vn',
+    port: 443,
+    path: '/v2/gateway/api/create',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(requestBody)
+    }
+}
+//Send the request and get the response
+const req = https.request(options, res => {
+    console.log(`Status: ${res.statusCode}`);
+    console.log(`Headers: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', (body) => {
+        console.log('Body: ');
+        console.log(body);
+        console.log('resultCode: ');
+        console.log(JSON.parse(body).resultCode);
+    });
+    res.on('end', () => {
+        console.log('No more data in response.');
+    });
+})
+
+req.on('error', (e) => {
+    console.log(`problem with request: ${e.message}`);
+});
+// write data to request body
+console.log("Sending....")
+req.write(requestBody);
+req.end();
